b836e29afb0912c21fadaea6be6bcba7c1cb7144
sivaprasadnvs <35561237+sivaprasadnvs@users.noreply.github.com>
2026-09-04 16:34:31 +0530

    DSA for loop

---
+++ DSAForLoop.ts
@@ -0,0 +1,11 @@
+for(let i= 0; i<100; i++){
+    console.log(i);
+}
+
+const evenArray = [1,5,10,22,6,3,7]
+
+for(let i=0; i< evenArray.length; i++){
+   if(evenArray[i] % 2 === 0){
+       console.log(evenArray[i]);
+   }
+}
